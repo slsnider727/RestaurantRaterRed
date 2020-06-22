c422f1144790d3cd86de907756375413eeb6bf3b
Sabra Snider <sabralsnider@gmail.com>
2020-06-22 16:09:05 -0400

    Add Details Functionality

--- RestaurantRaterRed/Controllers/RestaurantController.cs
+++ RestaurantRaterRed/Controllers/RestaurantController.cs
@@ -91,5 +91,19 @@ namespace RestaurantRaterRed.Controllers
             }
             return View(restaurant);
         }
+        //GET: Restaurant/Details/{id}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
     }
 }
